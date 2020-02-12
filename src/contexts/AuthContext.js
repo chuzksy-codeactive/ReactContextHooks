d240b83fb9d5314c979e9cd99418ed54ea41dc54
onuchukwuchika <chuzksy@gmail.com>
2020-02-12 07:32:44 +0100

    Convert the stateful class components to stateful functional components using context and hooks

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,23 +1,19 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
-class AuthenticateProvider extends Component {
-  state = {
-    isAuthenticated: false
-  };
+const AuthenticateProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  toggleAuth = () => {
-    this.setState({ isAuthenticated: !this.state.isAuthenticated });
-  };
-
-  render() {
-    return (
-      <AuthContext.Provider value={{ ...this.state, toggleAuth: this.toggleAuth }}>
-        {this.props.children}
-      </AuthContext.Provider>
-    );
+  const toggleAuth = () => {
+    setIsAuthenticated(!isAuthenticated);
   }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, toggleAuth }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthenticateProvider;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,25 +1,21 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
 
-class ThemeContextProvider extends Component {
-  state = {
-    isLightTheme: false,
-    light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
-    dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
-  }
+const ThemeContextProvider = (props) => {
+  const [isLightTheme, setIsLightTheme] = useState(false);
+  const [light] = useState({ syntax: '#555', ui: '#ddd', bg: '#eee' })
+  const [dark] = useState({ syntax: '#ddd', ui: '#333', bg: '#555' })
 
-  toggleTheme = () => {
-    this.setState({ isLightTheme: !this.state.isLightTheme });
-  }
+  const toggleTheme = () => {
+    setIsLightTheme(!isLightTheme);
+  };
 
-  render() {
-    return (
-      <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={{ isLightTheme, light, dark, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
 
 export default ThemeContextProvider;
